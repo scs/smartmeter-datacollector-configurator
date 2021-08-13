8447ce75df9fb2d26221e1b1df952ddb1bc57038
Mario Coray <mario.coray@scs.ch>
2021-08-13 15:24:15 +0200

    install icon package

--- frontend/smartmeter-datacollector-configurator/src/main.js
+++ frontend/smartmeter-datacollector-configurator/src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Buefy from "buefy";
+import "@fortawesome/fontawesome-free/css/solid.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
 import "buefy/dist/buefy.min.css";
 
 Vue.config.productionTip = false;
-Vue.use(Buefy);
+Vue.use(Buefy, {
+  defaultIconPack: "fas",
+});
 
 new Vue({
   render: (h) => h(App),
