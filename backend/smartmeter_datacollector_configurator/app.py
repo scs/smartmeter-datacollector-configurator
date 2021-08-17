e1ac263d4be92a70fff9e409e8530d63b52690fd
Mario Coray <mario.coray@scs.ch>
2021-08-17 12:58:46 +0200

    add basic authentication for all routes

--- backend/smartmeter_datacollector_configurator/app.py
+++ backend/smartmeter_datacollector_configurator/app.py
@@ -2,9 +2,11 @@ import logging
 
 from pydantic.error_wrappers import ValidationError
 from starlette.applications import Starlette
+from starlette.authentication import requires
 from starlette.endpoints import HTTPEndpoint
 from starlette.exceptions import HTTPException
 from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse, PlainTextResponse, Response
@@ -12,28 +14,28 @@ from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 
 from . import configurator
+from .authentication import BasicAuthBackend
 from .dto import ConfigDto
 
 DEFAULT_STATIC_DIR_PATH = "./static"
 DEFAULT_CONFIG_PATH = "./config.ini"
 
-logging.basicConfig(level=logging.DEBUG)
-
 
 # Endpoints
 
 class Configuration(HTTPEndpoint):
+    @requires("authenticated")
     async def get(self, request):
         dto = configurator.retrieve_config(DEFAULT_CONFIG_PATH)
         return JSONResponse(dto.json())
 
+    @requires("authenticated")
     async def post(self, request: Request):
         try:
             config = ConfigDto.parse_obj(await request.json())
         except ValidationError as e:
             logging.error(e)
             raise HTTPException(status_code=400, detail=str(e))
-        print(config)
         try:
             configurator.write_config_from_dto(DEFAULT_CONFIG_PATH, config)
         except Exception as e:
@@ -43,10 +45,12 @@ class Configuration(HTTPEndpoint):
         return Response("ok", media_type="text/plain")
 
 
+@requires("authenticated")
 async def restart(request):
     return PlainTextResponse()
 
 
+@requires("authenticated")
 async def set_credentials(request):
     return PlainTextResponse()
 
@@ -59,7 +63,8 @@ routes = [
 ]
 
 middleware = [
-    Middleware(CORSMiddleware, allow_origins=['*'])
+    Middleware(CORSMiddleware, allow_origins=['*']),
+    Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
 ]
 
 app = Starlette(debug=True, routes=routes, middleware=middleware)
---
+++ backend/smartmeter_datacollector_configurator/authentication.py
@@ -0,0 +1,79 @@
+import base64
+import binascii
+import logging
+
+from starlette.authentication import (AuthCredentials, AuthenticationBackend,
+                                      AuthenticationError, SimpleUser)
+from starlette.requests import HTTPConnection
+
+
+class SetPasswordError(Exception):
+    pass
+
+
+class AuthManager:
+    USERNAME = "admin"
+    DEFAULT_PASSWORD = "smartmeter"
+    PASSWORD_FILE_PATH = "./password.txt"
+
+    def __init__(self) -> None:
+        self._password = self._read_pwd_file(self.PASSWORD_FILE_PATH)
+
+    def check_credentials(self, username: str, password: str) -> bool:
+        if username == self.USERNAME and password == self._password:
+            logging.debug("User %s successfully authenticated.", username)
+            return True
+        logging.warning("User %s failed to authenticate.", username)
+        return False
+
+    def set_new_password(self, new_password: str) -> None:
+        self._password = new_password
+        self._write_pwd_file(new_password, self.PASSWORD_FILE_PATH)
+
+    @staticmethod
+    def _read_pwd_file(file_path: str) -> str:
+        try:
+            with open(file_path, "r") as file:
+                pwd = file.readline()
+                if not pwd.strip():
+                    raise ValueError("Password file is empty.")
+                return pwd
+        except (OSError, ValueError) as e:
+            logging.warning(f"Unable to read password file. '{e}' \n\tGenerating new file with default password.")
+            AuthManager._write_pwd_file(AuthManager.DEFAULT_PASSWORD, file_path)
+            return AuthManager.DEFAULT_PASSWORD
+
+    @staticmethod
+    def _write_pwd_file(password: str, file_path: str) -> None:
+        try:
+            with open(file_path, "w") as file:
+                file.write(password.strip())
+        except OSError as e:
+            logging.error(f"Unable to write password file. '{e}'")
+            raise SetPasswordError(e) from e
+
+
+auth_manager = AuthManager()
+
+
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, request: HTTPConnection):
+        if "Authorization" not in request.headers:
+            return
+
+        auth = request.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            print(scheme)
+            print(credentials)
+            if scheme.lower() != "basic":
+                return
+            decoded_credentials = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error):
+            raise AuthenticationError('Invalid credentials.')
+
+        username, _, password = decoded_credentials.partition(":")
+
+        if auth_manager.check_credentials(username, password):
+            return AuthCredentials(["authenticated"]), SimpleUser(username)
+        return None
