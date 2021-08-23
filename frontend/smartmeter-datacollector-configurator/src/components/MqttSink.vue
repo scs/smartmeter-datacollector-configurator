0d6e815ddce436ca03642aeb6bd2c1f99ea9d42e
Mario Coray <mario.coray@scs.ch>
2021-08-23 10:44:35 +0200

    frontend: add MQTT ca cert & authentication

--- frontend/smartmeter-datacollector-configurator/src/components/MqttSink.vue
+++ frontend/smartmeter-datacollector-configurator/src/components/MqttSink.vue
@@ -5,13 +5,32 @@
       <b-icon class="level-right" type="is-danger" icon="trash" @click.native.stop="$emit('remove')" />
     </p>
     <b-field label-position="on-border" label="Host (IP/Hostname)">
-      <b-input v-model="host" type="text" required placeholder="localhost" @input="update"></b-input>
+      <b-input v-model="host" type="text" required placeholder="localhost" lazy @input="update"></b-input>
     </b-field>
     <b-field label-position="on-border" label="Port">
       <b-input v-model.number="port" required type="number" min="1" max="65535" @input="update"></b-input>
     </b-field>
     <b-field>
       <b-checkbox v-model="tls" :value="false" @input="update">Use TLS protected connection</b-checkbox>
+    </b-field>
+    <b-field v-show="tls" label-position="on-border" label="CA Certificate (optional)">
+      <b-input
+        custom-class="is-family-monospace has-background-white-ter is-size-7"
+        v-model="caCert"
+        type="textarea"
+        placeholder="PEM formatted certificate"
+        lazy
+        @input="update"
+      ></b-input>
+    </b-field>
+    <b-field>
+      <b-checkbox v-model="authEnabled" :value="false" @input="update">Use Authentication</b-checkbox>
+    </b-field>
+    <b-field v-show="authEnabled" label-position="on-border" label="Username">
+      <b-input v-model="username" type="text" required lazy @input="update"></b-input>
+    </b-field>
+    <b-field v-show="authEnabled" label-position="on-border" label="Password (optional)">
+      <b-input type="password" v-model="password" lazy password-reveal @input="update"></b-input>
     </b-field>
   </div>
 </template>
@@ -29,6 +48,10 @@ export default {
       host: this.initConfig.host || "",
       port: this.initConfig.port || 1883,
       tls: "tls" in this.initConfig ? this.initConfig.tls : false,
+      caCert: this.initConfig.caCert || "",
+      authEnabled: this.initConfig.username != null,
+      username: this.initConfig.username || "",
+      password: this.initConfig.password || "",
     };
   },
   created() {
@@ -41,6 +64,9 @@ export default {
         host: this.host,
         port: this.port,
         tls: this.tls,
+        caCert: this.caCert && this.tls ? this.caCert : null,
+        username: this.authEnabled && this.username ? this.username : null,
+        password: this.authEnabled && this.password ? this.password : null,
       });
     },
   },
