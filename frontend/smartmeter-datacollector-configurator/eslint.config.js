cc956f382ad26dda3794d9898f0d1662aad668a9
Mario Coray <mario.coray@scs.ch>
2026-06-04 13:12:33 +0200

    frontend: update all dependencies to latest major version; exception: vite remained on version 7 due to new css minifier (lightningcss) that conflicts with buefy

--- frontend/smartmeter-datacollector-configurator/eslint.config.js
+++ frontend/smartmeter-datacollector-configurator/eslint.config.js
@@ -1,14 +1,21 @@
 import js from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
 import prettierConfig from "@vue/eslint-config-prettier";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
 
-export default [
+export default defineConfig([
   js.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   prettierConfig,
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       "vue/multi-word-component-names": "off",
     },
   },
-];
+]);
